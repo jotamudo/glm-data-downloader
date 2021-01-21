14b0016ba41959b134fda1c59216b9000fe00d24
jotamudo <juanvasconceloslopes@gmail.com>
2021-01-21 13:44:29 -0300

    changed test file

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ special_params = {
 
 tic = time()
 
-back.assets_download(start_params, end_params)
+# back.assets_download(start_params, end_params)
 back.data_acces(start_params, end_params)
 # back.generate_map(start_params, start_params, 1000, (-1.474012, -48.457615))
 tac = time()
